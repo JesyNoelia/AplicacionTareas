5985f06406eac6320577167812d6b28e3041d437
Jesy <jncepero@gmail.com>
2021-05-01 17:33:53 +0200

    array de tareas

--- js/tareas.js
+++ js/tareas.js
@@ -3,17 +3,17 @@ const tareas = new Array(
     {
         idTarea: 1,
         titulo: 'Ir al Gimnasio',
-        prioridad: 'Diaria'
+        prioridad: 'diaria'
     },
     {
         idTarea: 2,
         titulo: 'Ir al mercado',
-        prioridad: 'Mensual'
+        prioridad: 'mensual'
     },
     {
         idTarea: 3,
         titulo: 'Pagar impuestos',
-        prioridad: 'Urgente'
+        prioridad: 'urgente'
     }
 )
 
