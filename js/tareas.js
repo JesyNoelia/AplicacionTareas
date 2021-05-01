88257b9bb3a0a86c83e691f08c501673b3d4854c
Jesy <jncepero@gmail.com>
2021-05-01 16:15:02 +0200

    Modificacion de datos en el array

--- js/tareas.js
+++ js/tareas.js
@@ -3,17 +3,17 @@ const tareas = new Array(
     {
         idTarea: 1,
         titulo: 'Ir al Gimnasio',
-        prioridad: 'mensual'
+        prioridad: 'Diaria'
     },
     {
         idTarea: 2,
         titulo: 'Ir al mercado',
-        prioridad: 'diaria'
+        prioridad: 'Mensual'
     },
     {
         idTarea: 3,
         titulo: 'Pagar impuestos',
-        prioridad: 'urgente'
+        prioridad: 'Urgente'
     }
 )
 
