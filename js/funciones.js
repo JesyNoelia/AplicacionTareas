6c3dd6918629944ae2c75cd8cd1ba27fd1ef9302
Jesy <jncepero@gmail.com>
2021-05-01 16:14:30 +0200

    funciones de filtros

--- js/funciones.js
+++ js/funciones.js
@@ -2,3 +2,16 @@ function borraArray(pLista, pId) {
     let idABorrar = pLista.findIndex(tarea => tarea.idTarea === pId);
     tareas.splice(idABorrar, 1);
 }
+
+
+function filtrarPorPrioridad(pPrioridad, pListaTareas) {
+    const listaTareasPrioridad = pListaTareas.filter(tarea => tarea.prioridad.toLowerCase() === pPrioridad.toLowerCase());
+    return listaTareasPrioridad;
+}
+
+
+function buscarPorPalabra(pPalabra, pList) {
+    let palabra = pPalabra.toLowerCase().trim();
+    let listaPalabras = pList.filter(tarea => tarea.titulo.toLowerCase().includes(palabra));
+    return listaPalabras
+}
